99785fd6eea095786df9323a6ebc461fa684b739
dnosur <45685967+dnosur@users.noreply.github.com>
2024-08-11 19:42:40 +0300

    feat(GameEnd): created "GameEnd" header file

---
+++ Sapper/GameEnd.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// Диалоговое окно GameEnd
+
+class GameEnd : public CDialogEx
+{
+	DECLARE_DYNAMIC(GameEnd)
+
+	CFont m_fontGameOver;
+	bool retry, win;
+public:
+	GameEnd(bool win = false, CWnd* pParent = nullptr);   // стандартный конструктор
+	virtual ~GameEnd();
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG2 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // поддержка DDX/DDV
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnPaint();
+};
